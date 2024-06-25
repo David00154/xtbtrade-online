3fec07e5acdf97b8a4a97c760244fa65a0957341
david00154 <davidbriggs00154@gmail.com>
2024-06-25 13:04:47 +0100

    fixed mail issues

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,164 +2,170 @@ const { oneOf, body, query } = require("express-validator");
 const { prisma } = require("../utils/prisma.js");
 const nodemailer = require("nodemailer");
 const { emailTemplate } = require("../utils/emailTemplate");
+
+const { mailConfig } = require("../utils/mailConfig.js");
+
 const sendNotification = async (req, res, next) => {
-	const { email, title, body } = req.body;
-	// const user = await prisma.user.findUnique({
-	// 	where: {
-	// 		email: email,
-	// 	},
-	// });
+  const { email, title, body } = req.body;
+  // const user = await prisma.user.findUnique({
+  // 	where: {
+  // 		email: email,
+  // 	},
+  // });
 
-	// let date =
-	// 	new Date(Date.now()).getFullYear() +
-	// 	"-" +
-	// 	(new Date(Date.now()).getMonth() + 1) +
-	// 	"-" +
-	// 	new Date(Date.now()).getDate(); // yyyy-mm-dd
+  // let date =
+  // 	new Date(Date.now()).getFullYear() +
+  // 	"-" +
+  // 	(new Date(Date.now()).getMonth() + 1) +
+  // 	"-" +
+  // 	new Date(Date.now()).getDate(); // yyyy-mm-dd
 
-	// const sentNotification = await prisma.notification.create({
-	// 	data: {
-	// 		body,
-	// 		title,
-	// 		userId: user.id,
-	// 		date,
-	// 	},
-	// });
-	// console.log(sentNotification);
-	// // res.send("oks");
-	// req.flash("success_msg", "Notification sent");
-	// res.redirect("/admin/send-notification");
-	// next();
-	// console.log(req.body);
-	// req.flash("success_msg", "Email and notification sent boss");
-	// res.redirect("/admin/send-notification");
-	// next();
-	// console.log(body.replace("&#10", "<br />").replace("&#13", "<br />"));
-	// console.log(body)
+  // const sentNotification = await prisma.notification.create({
+  // 	data: {
+  // 		body,
+  // 		title,
+  // 		userId: user.id,
+  // 		date,
+  // 	},
+  // });
+  // console.log(sentNotification);
+  // // res.send("oks");
+  // req.flash("success_msg", "Notification sent");
+  // res.redirect("/admin/send-notification");
+  // next();
+  // console.log(req.body);
+  // req.flash("success_msg", "Email and notification sent boss");
+  // res.redirect("/admin/send-notification");
+  // next();
+  // console.log(body.replace("&#10", "<br />").replace("&#13", "<br />"));
+  // console.log(body)
 
-	let transporter = nodemailer.createTransport({
-		host: "mailbox.xtbtradingonline.space",
-		port: 465,
-		//   port: 587,
-		secure: true,
-		auth: {
-			user: "support-agent@mailbox.xtbtradingonline.space",
-			pass: "tUy0G,J4BZFX",
-		},
-	});
-	// console.log(body.match(/[\r\n|\r|\n]/gm))
-	// [\n]/gm
-	const captureNReplaceNewLine = () => {
-		let x = body;
-		body.match(/[\n]/gm).forEach((element) => {
-			x = x.replace(element, "<br />");
-		});
-		return x;
-	};
-	let mailOptions = {
-		from: `"Admin@ Xtb Online Trading"
-			<support-agent@mailbox.xtbtradingonline.space>`,
-		to: email,
-		subject: title,
-		// text: body,
-		html: emailTemplate({ body: captureNReplaceNewLine(), title: title, url: "xtbtradingonline.space" }),
-	};
-	transporter.sendMail(mailOptions, function (error, info) {
-		if (error != null) {
-			console.log(error.message);
-			console.log("There is an error");
-			//   res.status(500).json({error: "Internal Server Error"})
-			req.flash("error_msg", error.message);
-			res.redirect("/admin/send-notification");
-			next();
-		} else {
-			console.log("Email sent: " + info.response);
-			req.flash("success_msg", "Email and notification sent boss");
-			res.redirect("/admin/send-notification");
-			next();
-		}
-	});
-	// res.redirect("/admin/send-notification");
-	// next();
+  let transporter = nodemailer.createTransport(mailConfig);
+  // console.log(body.match(/[\r\n|\r|\n]/gm))
+  // [\n]/gm
+  const captureNReplaceNewLine = () => {
+    let x = body;
+    body.match(/[\n]/gm).forEach((element) => {
+      x = x.replace(element, "<br />");
+    });
+    return x;
+  };
+  let mailOptions = {
+    from: `"Admin@ Xtb Online Trading"
+			<${mailConfig.auth.user}>`,
+    to: email,
+    subject: title,
+    // text: body,
+    html: emailTemplate({
+      body: captureNReplaceNewLine(),
+      title: title,
+      url: "xtbfx.online",
+    }),
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error != null) {
+      console.log(error.message);
+      console.log("There is an error");
+      //   res.status(500).json({error: "Internal Server Error"})
+      req.flash("error_msg", error.message);
+      res.redirect("/admin/send-notification");
+      next();
+    } else {
+      console.log("Email sent: " + info.response);
+      req.flash("success_msg", "Email and notification sent boss");
+      res.redirect("/admin/send-notification");
+      next();
+    }
+  });
+  // res.redirect("/admin/send-notification");
+  // next();
 };
 
 const updateUserStat = async (req, res, next) => {
-	const { earning, balance, deposit, withdraws, uid } = req.body;
+  const { earning, balance, deposit, withdraws, uid } = req.body;
 
-	const updatedUser = await prisma.stat.update({
-		where: {
-			userId: uid,
-		},
-		data: {
-			balance,
-			earning,
-			deposit,
-			withdraws,
-		},
-	});
-	req.flash("success_msg", "User updated");
-	res.redirect("/admin/update-person");
-	next();
+  const updatedUser = await prisma.stat.update({
+    where: {
+      userId: uid,
+    },
+    data: {
+      balance,
+      earning,
+      deposit,
+      withdraws,
+    },
+  });
+  req.flash("success_msg", "User updated");
+  res.redirect("/admin/update-person");
+  next();
 };
 
 const deleteUser = async (req, res, next) => {
-	const { user_id } = req.params;
-	const deleteNotification = prisma.notification.deleteMany({
-		where: {
-			userId: user_id,
-		},
-	});
-	const deleteLatestTransaction = prisma.latestTransaction.deleteMany({
-		where: {
-			userId: user_id,
-		},
-	});
-	const deleteDeposits = prisma.deposit.deleteMany({
-		where: {
-			userId: user_id,
-		},
-	});
-	const deleteStat = prisma.stat.deleteMany({
-		where: {
-			userId: user_id,
-		},
-	});
+  const { user_id } = req.params;
+  const deleteNotification = prisma.notification.deleteMany({
+    where: {
+      userId: user_id,
+    },
+  });
+  const deleteLatestTransaction = prisma.latestTransaction.deleteMany({
+    where: {
+      userId: user_id,
+    },
+  });
+  const deleteDeposits = prisma.deposit.deleteMany({
+    where: {
+      userId: user_id,
+    },
+  });
+  const deleteStat = prisma.stat.deleteMany({
+    where: {
+      userId: user_id,
+    },
+  });
 
-	const deleteUser = prisma.user.delete({
-		where: {
-			id: user_id,
-		},
-	});
+  const deleteUser = prisma.user.delete({
+    where: {
+      id: user_id,
+    },
+  });
 
-	const transaction = await prisma.$transaction([deleteNotification, deleteDeposits, deleteLatestTransaction, deleteStat, deleteUser]);
-	console.log(transaction);
-	req.flash("success_msg", `User ${transaction.at(4).name} has been deleted`);
-	res.redirect("/admin/users");
-	next();
+  const transaction = await prisma.$transaction([
+    deleteNotification,
+    deleteDeposits,
+    deleteLatestTransaction,
+    deleteStat,
+    deleteUser,
+  ]);
+  console.log(transaction);
+  req.flash("success_msg", `User ${transaction.at(4).name} has been deleted`);
+  res.redirect("/admin/users");
+  next();
 };
 //
 const validateUpdateUserFields = oneOf([
-	[
-		body("uid").exists().withMessage("Enter a user id to update"),
-		body("earning").notEmpty().withMessage("Please add earning for the user"),
-		body("deposit").notEmpty().withMessage("Please add deposit for the user"),
-		body("balance").notEmpty().withMessage("Please add balance for the user"),
-		body("withdraws").notEmpty().withMessage("Please add withdraws for the user"),
-	],
+  [
+    body("uid").exists().withMessage("Enter a user id to update"),
+    body("earning").notEmpty().withMessage("Please add earning for the user"),
+    body("deposit").notEmpty().withMessage("Please add deposit for the user"),
+    body("balance").notEmpty().withMessage("Please add balance for the user"),
+    body("withdraws")
+      .notEmpty()
+      .withMessage("Please add withdraws for the user"),
+  ],
 ]);
 
 const validateNotificationsFields = oneOf([
-	[
-		body("title").notEmpty().withMessage("Title field is required"),
-		body("body").notEmpty().withMessage("Body field is required"),
-		body("email").notEmpty().withMessage("Please select a user email"),
-	],
+  [
+    body("title").notEmpty().withMessage("Title field is required"),
+    body("body").notEmpty().withMessage("Body field is required"),
+    body("email").notEmpty().withMessage("Please select a user email"),
+  ],
 ]);
 
 module.exports = {
-	validateNotificationsFields,
-	validateUpdateUserFields,
-	updateUserStat,
-	sendNotification,
-	deleteUser,
+  validateNotificationsFields,
+  validateUpdateUserFields,
+  updateUserStat,
+  sendNotification,
+  deleteUser,
 };
--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,260 +1,286 @@
 const express = require("express");
-const { ensureAuthenticated, validateWithdrawFormFields, validateErrors, validateDepositFormFields } = require("../controllers/authController.js");
+const {
+  ensureAuthenticated,
+  validateWithdrawFormFields,
+  validateErrors,
+  validateDepositFormFields,
+} = require("../controllers/authController.js");
 const nodemailer = require("nodemailer");
 const { prisma } = require("../utils/prisma.js");
 const { emailTemplate } = require("../utils/emailTemplate");
 const router = express.Router();
 router.use((req, res, next) => {
-	req.session.current_url = req.baseUrl + "" + req.url;
-	next();
+  req.session.current_url = req.baseUrl + "" + req.url;
+  next();
 });
+const { mailConfig } = require("../utils/mailConfig.js");
 
 router.route("/").get(ensureAuthenticated, async (req, res) => {
-	// console.log(req.user.id);
-	try {
-		let name = req.user.name.split(" ")[0][0].toUpperCase() + req.user.name.split(" ")[0].slice(1);
-		let user = req.user;
-		let email = req.user.email;
-		let {
-			stat: { balance, deposit, earning, withdraws },
-			notification,
-			latestTransactions,
-		} = await prisma.user.findUnique({
-			where: {
-				id: req.user.id,
-			},
-			select: {
-				stat: {
-					select: {
-						balance: true,
-						deposit: true,
-						earning: true,
-						withdraws: true,
-					},
-				},
-				latestTransactions: {
-					select: {
-						amount: true,
-						status: true,
-						date: true,
-					},
-				},
-				notification: {
-					select: {
-						title: true,
-						date: true,
-					},
-				},
-			},
-		});
-		res.render(
-			"backend/dashboard",
-			buildObject({
-				title: "Dashboard",
-				layout: "backend/layout",
-				user: req.user,
-				name,
-				earning,
-				deposit,
-				balance,
-				withdraws,
-				notification,
-				email: req.user.email,
-				extractScripts: true,
-				latestTransactions,
-			})
-		);
-	} catch (err) {
-		console.log(err);
-		req.flash("error_msg", "Error trying to locate the page, try loging in.");
-		res.redirect("/user/login");
-	}
+  // console.log(req.user.id);
+  try {
+    let name =
+      req.user.name.split(" ")[0][0].toUpperCase() +
+      req.user.name.split(" ")[0].slice(1);
+    let user = req.user;
+    let email = req.user.email;
+    let {
+      stat: { balance, deposit, earning, withdraws },
+      notification,
+      latestTransactions,
+    } = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+      select: {
+        stat: {
+          select: {
+            balance: true,
+            deposit: true,
+            earning: true,
+            withdraws: true,
+          },
+        },
+        latestTransactions: {
+          select: {
+            amount: true,
+            status: true,
+            date: true,
+          },
+        },
+        notification: {
+          select: {
+            title: true,
+            date: true,
+          },
+        },
+      },
+    });
+    res.render(
+      "backend/dashboard",
+      buildObject({
+        title: "Dashboard",
+        layout: "backend/layout",
+        user: req.user,
+        name,
+        earning,
+        deposit,
+        balance,
+        withdraws,
+        notification,
+        email: req.user.email,
+        extractScripts: true,
+        latestTransactions,
+      })
+    );
+  } catch (err) {
+    console.log(err);
+    req.flash("error_msg", "Error trying to locate the page, try loging in.");
+    res.redirect("/user/login");
+  }
 });
 router.route("/wallet").get(ensureAuthenticated, async (req, res) => {
-	// console.log(req.user.id);
-	try {
-		let name = req.user.name.split(" ")[0][0].toUpperCase() + req.user.name.split(" ")[0].slice(1);
-		let user = req.user;
-		let email = req.user.email;
-		let {
-			stat: { balance, deposit, earning, withdraws },
-			latestTransactions,
-			notification,
-		} = await prisma.user.findUnique({
-			where: {
-				id: req.user.id,
-			},
-			select: {
-				stat: {
-					select: {
-						balance: true,
-						deposit: true,
-						earning: true,
-						withdraws: true,
-					},
-				},
-				latestTransactions: {
-					select: {
-						amount: true,
-						status: true,
-						date: true,
-					},
-				},
-				notification: {
-					select: {
-						title: true,
-						date: true,
-					},
-				},
-			},
-		});
-		res.render(
-			"backend/wallet",
-			buildObject({
-				title: "Withdraw",
-				layout: "backend/layout",
-				user,
-				name,
-				email,
-				earning,
-				deposit,
-				balance,
-				withdraws,
-				latestTransactions,
-				notification,
-			})
-		);
-	} catch (err) {
-		console.log(err);
-		req.flash("error_msg", "Error trying to locate the page, try loging in.");
-		res.redirect("/user/login");
-	}
+  // console.log(req.user.id);
+  try {
+    let name =
+      req.user.name.split(" ")[0][0].toUpperCase() +
+      req.user.name.split(" ")[0].slice(1);
+    let user = req.user;
+    let email = req.user.email;
+    let {
+      stat: { balance, deposit, earning, withdraws },
+      latestTransactions,
+      notification,
+    } = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+      select: {
+        stat: {
+          select: {
+            balance: true,
+            deposit: true,
+            earning: true,
+            withdraws: true,
+          },
+        },
+        latestTransactions: {
+          select: {
+            amount: true,
+            status: true,
+            date: true,
+          },
+        },
+        notification: {
+          select: {
+            title: true,
+            date: true,
+          },
+        },
+      },
+    });
+    res.render(
+      "backend/wallet",
+      buildObject({
+        title: "Withdraw",
+        layout: "backend/layout",
+        user,
+        name,
+        email,
+        earning,
+        deposit,
+        balance,
+        withdraws,
+        latestTransactions,
+        notification,
+      })
+    );
+  } catch (err) {
+    console.log(err);
+    req.flash("error_msg", "Error trying to locate the page, try loging in.");
+    res.redirect("/user/login");
+  }
 });
 
 router
-	.route("/withdraw")
-	.get(ensureAuthenticated, async (req, res) => {
-		// console.log(req.user.id);
-		try {
-			let name = req.user.name.split(" ")[0][0].toUpperCase() + req.user.name.split(" ")[0].slice(1);
-			let user = req.user;
-			let email = req.user.email;
-			res.render(
-				"backend/withdraw",
-				buildObject({
-					title: "Withdraw",
-					layout: "backend/layout",
-					user,
-					name,
-					email,
-				})
-			);
-		} catch (err) {
-			console.log(err);
-			req.flash("error_msg", "Error trying to locate the page, try loging in.");
-			res.redirect("/user/login");
-		}
-	})
-	.post(ensureAuthenticated, validateWithdrawFormFields, validateErrors, async (req, res, next) => {
-		const { amount, coin_name, address } = req.body;
+  .route("/withdraw")
+  .get(ensureAuthenticated, async (req, res) => {
+    // console.log(req.user.id);
+    try {
+      let name =
+        req.user.name.split(" ")[0][0].toUpperCase() +
+        req.user.name.split(" ")[0].slice(1);
+      let user = req.user;
+      let email = req.user.email;
+      res.render(
+        "backend/withdraw",
+        buildObject({
+          title: "Withdraw",
+          layout: "backend/layout",
+          user,
+          name,
+          email,
+        })
+      );
+    } catch (err) {
+      console.log(err);
+      req.flash("error_msg", "Error trying to locate the page, try loging in.");
+      res.redirect("/user/login");
+    }
+  })
+  .post(
+    ensureAuthenticated,
+    validateWithdrawFormFields,
+    validateErrors,
+    async (req, res, next) => {
+      const { amount, coin_name, address } = req.body;
 
-		// let date =
-		// 	new Date(Date.now()).getFullYear() +
-		// 	"-" +
-		// 	(new Date(Date.now()).getMonth() + 1) +
-		// 	"-" +
-		// 	new Date(Date.now()).getDate(); // yyyy-mm-dd
-		// const latestTransaction =
-		// 	await prisma.latestTransaction.create({
-		// 		data: {
-		// 			amount,
-		// 			userId: req.user.id,
-		// 			status: false,
-		// 			date,
-		// 		},
-		// 	});
-		// console.log(req.body);
-		if (coin_name == "default") {
-			req.flash("error_msg", "Please select a coin name.");
-			res.redirect("/dashboard/withdraw");
-			next();
-		}
-		let transporter = nodemailer.createTransport({
-			host: "mailer.xtbtradingonline.space",
-			port: 465,
-			//   port: 587,
-			secure: true,
-			auth: {
-				user: "admin@mailer.xtbtradingonline.space",
-				pass: "5tuQkEC;=!w@",
-			},
-		});
-		var mailOptions = {
-			from: `"Bot@ Xtb Online Trading"
-					<admin@mailer.xtbtradingonline.space>`,
-			to: "admin@mailer.xtbtradingonline.space",
-			subject: "Client Withdraw Alert",
-			html: emailTemplate({
-				title: "Client Withdraw Alert",
-				url: "xtbtradingonline.space",
-				body: `A user with the email address ${req.user.email} has decided to make a withdrawal of '${amount}' to his/her ${coin_name} address with the addres of ${address}.`,
-			}),
-		};
-		transporter.sendMail(mailOptions, function (error, info) {
-			if (error != null) {
-				console.log(error.message);
-				console.log("There is an error");
-				//   res.status(500).json({error: "Internal Server Error"})
-				req.flash("error_msg", "Internal server error");
-				res.redirect("/dashboard/withdraw");
-				next();
-			} else {
-				console.log("Email sent: " + info.response);
-				req.flash("success_msg", "We've sent your request to the admin. We will respond back via email.");
-				res.redirect("/dashboard/withdraw");
-				next();
-			}
-		});
-	});
+      // let date =
+      // 	new Date(Date.now()).getFullYear() +
+      // 	"-" +
+      // 	(new Date(Date.now()).getMonth() + 1) +
+      // 	"-" +
+      // 	new Date(Date.now()).getDate(); // yyyy-mm-dd
+      // const latestTransaction =
+      // 	await prisma.latestTransaction.create({
+      // 		data: {
+      // 			amount,
+      // 			userId: req.user.id,
+      // 			status: false,
+      // 			date,
+      // 		},
+      // 	});
+      // console.log(req.body);
+      if (coin_name == "default") {
+        req.flash("error_msg", "Please select a coin name.");
+        res.redirect("/dashboard/withdraw");
+        next();
+      }
+      let transporter = nodemailer.createTransport(mailConfig);
+      var mailOptions = {
+        from: `"Bot@ Xtb Online Trading"
+					<${mailConfig.auth.user}>`,
+        to: mailConfig.auth.user,
+        subject: "Client Withdraw Alert",
+        html: emailTemplate({
+          title: "Client Withdraw Alert",
+          url: "xtbfx.online",
+          body: `A user with the email address ${req.user.email} has decided to make a withdrawal of '${amount}' to his/her ${coin_name} address with the addres of ${address}.`,
+        }),
+      };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error != null) {
+          console.log(error.message);
+          console.log("There is an error");
+          //   res.status(500).json({error: "Internal Server Error"})
+          req.flash("error_msg", "Internal server error");
+          res.redirect("/dashboard/withdraw");
+          next();
+        } else {
+          console.log("Email sent: " + info.response);
+          req.flash(
+            "success_msg",
+            "We've sent your request to the admin. We will respond back via email."
+          );
+          res.redirect("/dashboard/withdraw");
+          next();
+        }
+      });
+    }
+  );
 
 router
-	.route("/deposit")
-	.get(ensureAuthenticated, async (req, res) => {
-		try {
-			let name = req.user.name.split(" ")[0][0].toUpperCase() + req.user.name.split(" ")[0].slice(1);
-			let user = req.user;
-			let email = req.user.email;
-			res.render(
-				"backend/deposit",
-				buildObject({
-					title: "Deposit",
-					layout: "backend/layout",
-					user,
-					name,
-					email,
-				})
-			);
-		} catch (err) {
-			console.log(err);
-			req.flash("error_msg", "Error trying to locate the page, try loging in.");
-			res.redirect("/user/login");
-		}
-	})
-	.post(ensureAuthenticated, validateDepositFormFields, validateErrors, async (req, res) => {
-		const { amount, address } = req.body;
-		let date = new Date(Date.now()).getFullYear() + "-" + (new Date(Date.now()).getMonth() + 1) + "-" + new Date(Date.now()).getDate(); // yyyy-mm-dd
-		const latestTransaction = await prisma.deposit.create({
-			data: {
-				userId: req.user.id,
-				address,
-				amount,
-				date,
-				recieved: false,
-			},
-		});
-		req.flash("success_msg", "We will update your assets when our systems has verified the deposit.");
-		res.redirect("/dashboard/deposit");
-	});
+  .route("/deposit")
+  .get(ensureAuthenticated, async (req, res) => {
+    try {
+      let name =
+        req.user.name.split(" ")[0][0].toUpperCase() +
+        req.user.name.split(" ")[0].slice(1);
+      let user = req.user;
+      let email = req.user.email;
+      res.render(
+        "backend/deposit",
+        buildObject({
+          title: "Deposit",
+          layout: "backend/layout",
+          user,
+          name,
+          email,
+        })
+      );
+    } catch (err) {
+      console.log(err);
+      req.flash("error_msg", "Error trying to locate the page, try loging in.");
+      res.redirect("/user/login");
+    }
+  })
+  .post(
+    ensureAuthenticated,
+    validateDepositFormFields,
+    validateErrors,
+    async (req, res) => {
+      const { amount, address } = req.body;
+      let date =
+        new Date(Date.now()).getFullYear() +
+        "-" +
+        (new Date(Date.now()).getMonth() + 1) +
+        "-" +
+        new Date(Date.now()).getDate(); // yyyy-mm-dd
+      const latestTransaction = await prisma.deposit.create({
+        data: {
+          userId: req.user.id,
+          address,
+          amount,
+          date,
+          recieved: false,
+        },
+      });
+      req.flash(
+        "success_msg",
+        "We will update your assets when our systems has verified the deposit."
+      );
+      res.redirect("/dashboard/deposit");
+    }
+  );
 
 // router
 // 	.route("/notifications")
@@ -274,88 +300,90 @@ router
 // 	});
 
 router.route("/markets").get(ensureAuthenticated, async (req, res) => {
-	// console.log(req.user.id);
-	try {
-		let name = req.user.name.split(" ")[0][0].toUpperCase() + req.user.name.split(" ")[0].slice(1);
-		let user = req.user;
-		let email = req.user.email;
-		let {
-			stat: { balance, deposit, earning, withdraws },
-			notification,
-		} = await prisma.user.findUnique({
-			where: {
-				id: req.user.id,
-			},
-			select: {
-				stat: {
-					select: {
-						balance: true,
-						deposit: true,
-						earning: true,
-						withdraws: true,
-					},
-				},
-				notification: {
-					select: {
-						title: true,
-						date: true,
-					},
-				},
-			},
-		});
-		res.render(
-			"backend/markets",
-			buildObject({
-				title: "Markets",
-				layout: "backend/layout",
-				user: req.user,
-				name,
-				// earning,
-				// deposit,
-				// balance,
-				// withdraws,
-				notification,
-				email: req.user.email,
-				extractScripts: true,
-			})
-		);
-	} catch (err) {
-		console.log(err);
-		req.flash("error_msg", "Error trying to locate the page, try loging in.");
-		res.redirect("/user/login");
-	}
+  // console.log(req.user.id);
+  try {
+    let name =
+      req.user.name.split(" ")[0][0].toUpperCase() +
+      req.user.name.split(" ")[0].slice(1);
+    let user = req.user;
+    let email = req.user.email;
+    let {
+      stat: { balance, deposit, earning, withdraws },
+      notification,
+    } = await prisma.user.findUnique({
+      where: {
+        id: req.user.id,
+      },
+      select: {
+        stat: {
+          select: {
+            balance: true,
+            deposit: true,
+            earning: true,
+            withdraws: true,
+          },
+        },
+        notification: {
+          select: {
+            title: true,
+            date: true,
+          },
+        },
+      },
+    });
+    res.render(
+      "backend/markets",
+      buildObject({
+        title: "Markets",
+        layout: "backend/layout",
+        user: req.user,
+        name,
+        // earning,
+        // deposit,
+        // balance,
+        // withdraws,
+        notification,
+        email: req.user.email,
+        extractScripts: true,
+      })
+    );
+  } catch (err) {
+    console.log(err);
+    req.flash("error_msg", "Error trying to locate the page, try loging in.");
+    res.redirect("/user/login");
+  }
 });
 
 function buildObject(obj) {
-	let {
-		name = "",
-		user = "",
-		title = "",
-		deposit = "",
-		earning = "",
-		balance = "",
-		withdraws = "",
-		email = "",
-		extractScripts = true,
-		latestTransactions = [],
-		notification = [],
-		// success_msg = "",
-	} = obj;
-	return {
-		...obj,
-		name,
-		user,
-		title,
-		deposit,
-		earning,
-		balance,
-		withdraws,
-		email,
-		extractScripts,
-		latestTransactions,
-		notifications: notification,
-		// success_msg,
-	};
+  let {
+    name = "",
+    user = "",
+    title = "",
+    deposit = "",
+    earning = "",
+    balance = "",
+    withdraws = "",
+    email = "",
+    extractScripts = true,
+    latestTransactions = [],
+    notification = [],
+    // success_msg = "",
+  } = obj;
+  return {
+    ...obj,
+    name,
+    user,
+    title,
+    deposit,
+    earning,
+    balance,
+    withdraws,
+    email,
+    extractScripts,
+    latestTransactions,
+    notifications: notification,
+    // success_msg,
+  };
 }
 
 module.exports = router;
